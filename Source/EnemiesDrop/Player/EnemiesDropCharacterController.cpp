a099c267d376c0db7d308173375bc4213d180d21
KGW2027 <retryer_@naver.com>
2022-10-09 00:57:18 +0900

    add playercontroller class 유지보수 관리를 위해 PlayerController 클래스를 추가했습니다.

---
+++ Source/EnemiesDrop/Player/EnemiesDropCharacterController.cpp
@@ -0,0 +1,92 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "EnemiesDropCharacterController.h"
+
+#include "EnemiesDrop/RPGCharacter.h"
+
+
+AEnemiesDropCharacterController::AEnemiesDropCharacterController()
+{
+	CameraTurnRate = 50.0f;
+	bIsJumping = false;
+	bIsRunning = false;
+}
+
+
+void AEnemiesDropCharacterController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	InputComponent->BindAction("Jump", IE_Pressed, this, &AEnemiesDropCharacterController::Jump);
+	InputComponent->BindAction("Jump", IE_Released, this, &AEnemiesDropCharacterController::Jump);
+
+	InputComponent->BindAction("Run", IE_Pressed, this, &AEnemiesDropCharacterController::Run);
+	InputComponent->BindAction("Run", IE_Released, this, &AEnemiesDropCharacterController::Run);
+
+	InputComponent->BindAxis("Move Forward / Backward", this, &AEnemiesDropCharacterController::MoveFB);
+	InputComponent->BindAxis("Move Right / Left", this, &AEnemiesDropCharacterController::MoveLR);
+
+	InputComponent->BindAxis("Turn Right / Left Mouse", this, &AEnemiesDropCharacterController::InputYaw);
+	InputComponent->BindAxis("Look Up / Down Mouse", this, &AEnemiesDropCharacterController::InputPitch);
+	
+}
+
+void AEnemiesDropCharacterController::Jump()
+{
+	if(!bIsJumping)
+		Cast<ACharacter>(GetPawn())->Jump();
+	else
+		Cast<ACharacter>(GetPawn())->StopJumping();
+	
+	bIsJumping = !bIsJumping;
+}
+
+void AEnemiesDropCharacterController::Run()
+{
+	if(!bIsRunning)
+		Cast<ARPGCharacter>(GetPawn())->ToggleSprint();
+	else
+		Cast<ARPGCharacter>(GetPawn())->ToggleSprint();
+
+	bIsRunning = !bIsRunning;
+}
+
+void AEnemiesDropCharacterController::MoveFB(float Axis)
+{
+	if (Axis != 0.0f)
+	{
+		// find out which way is forward
+		const FRotator Rotation = GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		// get forward vector
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+		GetPawn()->AddMovementInput(Direction, Axis);
+		// AddMovementInput(Direction, Value);
+	}
+}
+
+void AEnemiesDropCharacterController::MoveLR(float Axis)
+{
+	if(Axis != .0f)
+	{
+		const FRotator Rotation = GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+	
+		// get right vector 
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+		GetPawn()->AddMovementInput(Direction, Axis);
+	}
+}
+
+void AEnemiesDropCharacterController::InputYaw(float Axis)
+{
+	AddYawInput(Axis * CameraTurnRate * GetWorld()->GetDeltaSeconds());
+}
+
+void AEnemiesDropCharacterController::InputPitch(float Axis)
+{
+	AddPitchInput(Axis * CameraTurnRate * GetWorld()->GetDeltaSeconds());
+}
+
---
+++ Source/EnemiesDrop/Player/EnemiesDropCharacterController.h
@@ -0,0 +1,39 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "EnemiesDropCharacterController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ENEMIESDROP_API AEnemiesDropCharacterController : public APlayerController
+{
+	GENERATED_BODY()
+
+	AEnemiesDropCharacterController();
+
+protected:
+	UPROPERTY(BlueprintReadWrite)
+	float CameraTurnRate;
+
+	UPROPERTY(BlueprintReadOnly)
+	bool bIsJumping;
+
+	UPROPERTY(BlueprintReadOnly)
+	bool bIsRunning;
+
+public:
+	virtual void SetupInputComponent() override;
+
+	void Jump();
+	void Run();
+	void MoveFB(float Axis);
+	void MoveLR(float Axis);
+	void InputYaw(float Axis);
+	void InputPitch(float Axis);
+	
+};
